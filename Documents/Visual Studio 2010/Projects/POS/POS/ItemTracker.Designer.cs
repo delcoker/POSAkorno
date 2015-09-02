729c1c3f86c2acf4c4bf951ba97640f1f23a972f
Del Coker <delcoker@gmail.com>
2015-09-02 11:27:30 +0000

    search filter on datagridview

---
+++ Documents/Visual Studio 2010/Projects/POS/POS/ItemTracker.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace POS
+{
+    partial class ItemTracker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbItems = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnTrack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.cmbQty = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbPrice = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbItems
+            // 
+            this.cmbItems.FormattingEnabled = true;
+            this.cmbItems.Location = new System.Drawing.Point(12, 67);
+            this.cmbItems.Name = "cmbItems";
+            this.cmbItems.Size = new System.Drawing.Size(323, 32);
+            this.cmbItems.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.cmbPrice);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.cmbQty);
+            this.panel1.Controls.Add(this.cmbCategory);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnTrack);
+            this.panel1.Controls.Add(this.cmbItems);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 282);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(412, 200);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(191, 70);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnTrack
+            // 
+            this.btnTrack.Location = new System.Drawing.Point(12, 200);
+            this.btnTrack.Name = "btnTrack";
+            this.btnTrack.Size = new System.Drawing.Size(179, 70);
+            this.btnTrack.TabIndex = 1;
+            this.btnTrack.Text = "Track";
+            this.btnTrack.UseVisualStyleBackColor = true;
+            this.btnTrack.Click += new System.EventHandler(this.btnTrack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Item";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(500, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Category";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(504, 67);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(99, 32);
+            this.cmbCategory.TabIndex = 5;
+            // 
+            // cmbQty
+            // 
+            this.cmbQty.FormattingEnabled = true;
+            this.cmbQty.Location = new System.Drawing.Point(370, 67);
+            this.cmbQty.Name = "cmbQty";
+            this.cmbQty.Size = new System.Drawing.Size(99, 32);
+            this.cmbQty.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(366, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quantity";
+            // 
+            // cmbPrice
+            // 
+            this.cmbPrice.FormattingEnabled = true;
+            this.cmbPrice.Location = new System.Drawing.Point(504, 144);
+            this.cmbPrice.Name = "cmbPrice";
+            this.cmbPrice.Size = new System.Drawing.Size(99, 32);
+            this.cmbPrice.TabIndex = 8;
+            this.cmbPrice.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(212, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 70);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Stop Tracking";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ItemTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 282);
+            this.Controls.Add(this.panel1);
+            this.Name = "ItemTracker";
+            this.Text = "ItemTracker";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbItems;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnTrack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbQty;
+        private System.Windows.Forms.ComboBox cmbPrice;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Documents/Visual Studio 2010/Projects/POS/POS/ItemTracker.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POS
+{
+    public partial class ItemTracker : Form
+    {
+        private cUsers loggedUser;
+
+        public ItemTracker(cUsers user)
+        {
+            InitializeComponent();
+
+            LoadItems();
+
+
+            loggedUser = new cUsers();
+            loggedUser = user;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoadItems()
+        {
+            DataSet ds = new DataSet();
+            cMeals cat = new cMeals();
+
+            ds = cat.mealsGetActive();
+
+            //ds.Tables[0].DefaultView.Sort = "Category asc";
+
+            cmbItems.DataSource = ds.Tables[0].DefaultView;
+            cmbItems.ValueMember = "MealID";
+            cmbItems.DisplayMember = "Name";
+
+            cmbCategory.DataSource = cmbItems.DataSource;
+            cmbCategory.ValueMember = "CategoryID";
+            cmbCategory.DisplayMember = "Category";
+
+            cmbQty.DataSource = cmbItems.DataSource;
+            cmbQty.ValueMember = "QuanTypeID";
+            cmbQty.DisplayMember = "QuanType";
+
+            cmbPrice.DataSource = cmbItems.DataSource;
+            cmbPrice.ValueMember = "Price";
+            cmbPrice.DisplayMember = "Price";
+            //cmbItems.SelectedIndex = -1;
+        }
+
+        private void btnTrack_Click(object sender, EventArgs e)
+        {
+            cMeals meal = new cMeals();
+            meal.MealID = Convert.ToUInt32(cmbItems.SelectedValue);
+            meal.Deleted = false;
+            meal.Name = Convert.ToString(cmbItems.Text);
+            meal.Price = Convert.ToDecimal(cmbPrice.SelectedValue);
+            meal.CategoryID = Convert.ToUInt32(cmbCategory.SelectedValue);
+            meal.QuanTypeID = Convert.ToUInt32(cmbQty.SelectedValue);
+            meal.DateAdded = DateTime.Now.ToString();
+            meal.HasStock = true;
+
+
+            if (meal.saveRecord())
+            {
+                MessageBox.Show("Tracking " + meal.Name + ".\nYou may add stock.");
+
+            }
+            else
+                MessageBox.Show("Not Tracking");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cMeals meal = new cMeals();
+            meal.MealID = Convert.ToUInt32(cmbItems.SelectedValue);
+            meal.Deleted = false;
+            meal.Name = Convert.ToString(cmbItems.Text);
+            meal.Price = Convert.ToDecimal(cmbPrice.SelectedValue);
+            meal.CategoryID = Convert.ToUInt32(cmbCategory.SelectedValue);
+            meal.QuanTypeID = Convert.ToUInt32(cmbQty.SelectedValue);
+            meal.DateAdded = DateTime.Now.ToString();
+            meal.HasStock = false;
+
+
+            if (meal.saveRecord())
+            {
+                MessageBox.Show("Not Tracking " + meal.Name + ".\nYou may add stock.");
+
+            }
+            else
+                MessageBox.Show("Tracking");
+        }
+
+        
+    }
+}
